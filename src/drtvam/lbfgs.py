73553c149ff3032f88dc7d0be2a7f2191aa61ff2
Baptiste Nicolet <bnicolet@nvidia.com>
2025-08-12 18:44:01 +0200

    Update LBFGS implementation to be consistent with Mitsuba 3.7.0

--- src/drtvam/lbfgs.py
+++ src/drtvam/lbfgs.py
@@ -9,73 +9,53 @@ class LBFGS(mi.ad.Optimizer):
         self.wolfe = wolfe
         # History size
         self.m = m
-        # Previous state
-        self.p_old = {}
-        # Previous gradient
-        self.g_old = {}
-        # List of state differences
-        self.s = {}
-        # List of gradient differences
-        self.y = {}
-        # List of dot products of 'y' and 's'
-        self.ys = {}
-        # Iteration count
-        self.t = {}
         # Function used for line search, should take a dictionary of variables
         # as input and return the loss and gradients for each variable
         self.line_search_fn = line_search_fn
         # How many iterations of backtracking line search to run
         self.search_it = search_it
 
-    def reset(self, k):
-        self.s[k] = []
-        self.y[k] = []
-        self.ys[k] = []
-        self.t[k] = 0
+    def _reset(self, key, value, promoted):
+        self.state[key] = value, promoted, None, ([], [], [], 0, None, None)
 
     def update_history(self, k):
         # Discard too old entries
-        if self.t[k] > self.m:
-            self.s[k].pop(0)
-            self.y[k].pop(0)
-            self.ys[k].pop(0)
+        val, promoted, lr, (s, y, ys, t, p_old, g_old) = self.state[k]
 
-        p = dr.ravel(dr.detach(self.variables[k]))
-        g_p = dr.ravel(self.variables[k].grad)
+        if t > self.m:
+            s.pop(0)
+            y.pop(0)
+            ys.pop(0)
+
+        p = dr.ravel(dr.detach(val))
+        g_p = dr.ravel(val.grad)
         dr.schedule(p, g_p)
         # Update history
-        if self.t[k] > 0:
-            self.s[k].append(p - self.p_old[k])
-            self.y[k].append(g_p - self.g_old[k])
-            dr.schedule(self.s[k][-1], self.y[k][-1])
-            self.ys[k].append(dr.dot(self.y[k][-1], self.s[k][-1]))
+        if t > 0:
+            s.append(p - p_old)
+            y.append(g_p - g_old)
+            dr.schedule(s[-1], y[-1])
+            ys.append(dr.dot(y[-1], s[-1]))
 
         # Update previous state and gradient
-        self.p_old[k] = p
-        self.g_old[k] = g_p
-        self.t[k] += 1
+        self.state[k] = val, promoted, lr, (s, y, ys, t+1, p, g_p)
 
     def step(self, f):
         search_dirs = {}
         # TODO: unravel arrays for dot products
 
         # Compute search directions
-        for k, p in self.variables.items():
+        for k in self.state.keys():
             #TODO: Stopping criterion ?
-            # TODO: reset if shape changed
-            if k not in self.s:
-                self.reset(k)
 
             # Update history
             self.update_history(k)
+            p, promoted, lr, (s, y, ys, t, p_old, g_old) = self.state[k]
 
             # Find search direction
             q = dr.ravel(p.grad)
-            hist_size = len(self.s[k])
+            hist_size = len(s)
             dr.make_opaque(hist_size)
-            s = self.s[k]
-            y = self.y[k]
-            ys = self.ys[k]
             alphas = []
             for i in range(hist_size-1, -1, -1):
                 rho = dr.rcp(ys[i])
@@ -83,7 +63,7 @@ class LBFGS(mi.ad.Optimizer):
                 q = q - a * y[i]
                 alphas.insert(0, a)
 
-            gamma = 1 if self.t[k] == 1 else ys[-1] / dr.dot(y[-1], y[-1])
+            gamma = 1 if t == 1 else ys[-1] / dr.dot(y[-1], y[-1])
 
             z = gamma * q
             for i in range(hist_size):
@@ -101,7 +81,7 @@ class LBFGS(mi.ad.Optimizer):
         c2 = 0.9
         for it in range(self.search_it):
             params = {}
-            for k, p in self.variables.items():
+            for k, (p, _, _, _) in self.state.items():
                 if type(p) == mi.TensorXf:
                     params[k] = dr.detach(p + alpha * type(p)(search_dirs[k], shape=p.shape))
                 else:
@@ -118,8 +98,10 @@ class LBFGS(mi.ad.Optimizer):
 
             g_dot_z = mi.Float(0.)
             g_new_dot_z = mi.Float(0.)
-            for k, p in params.items():
-                g_dot_z += dr.dot(self.g_old[k], search_dirs[k])
+            for k in self.state.keys():
+                p = self.state[k][0]
+                g_old = self.state[k][3][5]
+                g_dot_z += dr.dot(g_old, search_dirs[k])
                 if self.wolfe:
                     g_new_dot_z += dr.dot(dr.ravel(p.grad), search_dirs[k])
 
@@ -134,32 +116,22 @@ class LBFGS(mi.ad.Optimizer):
 
             alpha *= 0.5
 
-        for k, p in self.variables.items():
+        for k, (p, promoted, lr, extra) in self.state.items():
             if type(p) == mi.TensorXf:
-                self.variables[k] = dr.detach(p + alpha * type(p)(search_dirs[k], shape=p.shape))
+                new_val = dr.detach(p + alpha * type(p)(search_dirs[k], shape=p.shape))
             else:
-                self.variables[k] = dr.detach(p + alpha * dr.unravel(type(p), search_dirs[k]))
-            # self.variables[k] = type(p)(dr.detach(p + alpha * dr.unravel(type(p), search_dirs[k])))
-            dr.enable_grad(self.variables[k])
-            dr.schedule(self.variables[k])
+                new_val = dr.detach(p + alpha * dr.unravel(type(p), search_dirs[k]))
+
+            dr.enable_grad(new_val)
+            new_state = new_val, promoted, lr, extra
+            dr.schedule(new_state)
+            self.state[k] = new_state
 
 class LinearLBFGS(mi.ad.Optimizer):
     def __init__(self, lr=1.0, m=5, params=None, render_fn=None, loss_fn=None, search_it=20):
         super().__init__(lr, params)
         # History size
         self.m = m
-        # Previous state
-        self.p_old = {}
-        # Previous gradient
-        self.g_old = {}
-        # List of state differences
-        self.s = {}
-        # List of gradient differences
-        self.y = {}
-        # List of dot products of 'y' and 's'
-        self.ys = {}
-        # Iteration count
-        self.t = {}
         # Function used for line search, should take a dictionary of variables
         # as input and return the loss and gradients for each variable
         self.render_fn = render_fn
@@ -167,55 +139,48 @@ class LinearLBFGS(mi.ad.Optimizer):
         # How many iterations of backtracking line search to run
         self.search_it = search_it
 
-    def reset(self, k):
-        self.s[k] = []
-        self.y[k] = []
-        self.ys[k] = []
-        self.t[k] = 0
+    def _reset(self, key, value, promoted):
+        self.state[key] = value, promoted, None, ([], [], [], 0, None, None)
 
     def update_history(self, k):
         # Discard too old entries
-        if self.t[k] > self.m:
-            self.s[k].pop(0)
-            self.y[k].pop(0)
-            self.ys[k].pop(0)
+        val, promoted, lr, (s, y, ys, t, p_old, g_old) = self.state[k]
 
-        p = dr.ravel(dr.detach(self.variables[k]))
-        g_p = dr.ravel(self.variables[k].grad)
+        if t > self.m:
+            s.pop(0)
+            y.pop(0)
+            ys.pop(0)
+
+        p = dr.ravel(dr.detach(val))
+        g_p = dr.ravel(val.grad)
         dr.schedule(p, g_p)
         # Update history
-        if self.t[k] > 0:
-            self.s[k].append(p - self.p_old[k])
-            self.y[k].append(g_p - self.g_old[k])
-            dr.schedule(self.s[k][-1], self.y[k][-1])
-            self.ys[k].append(dr.dot(self.y[k][-1], self.s[k][-1]))
+        if t > 0:
+            s.append(p - p_old)
+            y.append(g_p - g_old)
+            dr.schedule(s[-1], y[-1])
+            ys.append(dr.dot(y[-1], s[-1]))
 
         # Update previous state and gradient
-        self.p_old[k] = p
-        self.g_old[k] = g_p
-        self.t[k] += 1
+        self.state[k] = val, promoted, lr, (s, y, ys, t+1, p, g_p)
 
     def step(self, vol, loss):
         search_dirs = {}
         # TODO: unravel arrays for dot products
 
         # Compute search directions
-        for k, p in self.variables.items():
+        for k in self.state.keys():
             #TODO: Stopping criterion ?
-            # TODO: reset if shape changed
-            if k not in self.s:
-                self.reset(k)
 
             # Update history
             self.update_history(k)
 
+            p, promoted, lr, (s, y, ys, t, p_old, g_old) = self.state[k]
+
             # Find search direction
             q = dr.ravel(p.grad)
-            hist_size = len(self.s[k])
+            hist_size = len(s)
             dr.make_opaque(hist_size)
-            s = self.s[k]
-            y = self.y[k]
-            ys = self.ys[k]
             alphas = []
             for i in range(hist_size-1, -1, -1):
                 rho = dr.rcp(ys[i])
@@ -223,7 +188,7 @@ class LinearLBFGS(mi.ad.Optimizer):
                 q = q - a * y[i]
                 alphas.insert(0, a)
 
-            gamma = 1 if self.t[k] == 1 else ys[-1] / dr.dot(y[-1], y[-1])
+            gamma = 1 if t == 1 else ys[-1] / dr.dot(y[-1], y[-1])
 
             z = gamma * q
             for i in range(hist_size):
@@ -237,11 +202,12 @@ class LinearLBFGS(mi.ad.Optimizer):
         alpha = dr.opaque(mi.Float, 1.) # TODO: expose initial step size as param
         c1 = 1e-4
         params = {}
-        for k, p in self.variables.items():
-            if type(p) == mi.TensorXf:
-                params[k] = dr.detach(type(p)(search_dirs[k], shape=p.shape))
+        for k in self.state.keys():
+            tp = type(self.state[k][0])
+            if tp == mi.TensorXf:
+                params[k] = dr.detach(tp(search_dirs[k], shape=p.shape))
             else:
-                params[k] = dr.detach(dr.unravel(type(p), search_dirs[k]))
+                params[k] = dr.detach(dr.unravel(tp, search_dirs[k]))
             #TODO: clamping ?
             dr.schedule(params[k])
 
@@ -250,8 +216,9 @@ class LinearLBFGS(mi.ad.Optimizer):
 
         # Compute dot product of gradient and search direction for Armijo condition
         g_dot_z = mi.Float(0.)
-        for k, p in params.items():
-            g_dot_z += dr.dot(self.g_old[k], search_dirs[k])
+        for k in self.state.keys():
+            g_old = self.state[k][3][5]
+            g_dot_z += dr.dot(g_old, search_dirs[k])
 
         for _ in range(self.search_it):
 
@@ -265,11 +232,14 @@ class LinearLBFGS(mi.ad.Optimizer):
 
             alpha *= 0.5
 
-        for k, p in self.variables.items():
+        for k, (p, promoted, lr, extra) in self.state.items():
             if type(p) == mi.TensorXf:
-                self.variables[k] = dr.detach(p + alpha * type(p)(search_dirs[k], shape=p.shape))
+                new_val = dr.detach(p + alpha * type(p)(search_dirs[k], shape=p.shape))
             else:
-                self.variables[k] = dr.detach(p + alpha * dr.unravel(type(p), search_dirs[k]))
-            # self.variables[k] = type(p)(dr.detach(p + alpha * dr.unravel(type(p), search_dirs[k])))
-            dr.enable_grad(self.variables[k])
-            dr.schedule(self.variables[k])
+                new_val = dr.detach(p + alpha * dr.unravel(type(p), search_dirs[k]))
+
+            dr.enable_grad(new_val)
+            new_state = new_val, promoted, lr, extra
+            dr.schedule(new_state)
+            self.state[k] = new_state
+
